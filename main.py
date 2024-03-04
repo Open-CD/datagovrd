42e94bb0662c8841ac758fe270a979e590510ae3
LudwigCespedes <ludwigcespedesogando@gmail.com>
2024-03-04 19:37:07 -0400

    Generar gráficas de diferentes categorías.
    Se ha agregado un archivo en la ruta `/services` que crea gráficos a partir de los datos sin procesar. Este archivo brinda una funcionalidad adicional al proyecto al permitir la visualización de datos sin la necesidad de realizar una limpieza previa. Invito a revisar la documentación del archivo para comprender su funcionamiento y probar la clase. Si encuentra algún problema o tienes alguna pregunta, no dudes en contactarme para solucionarlo.

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ app = FastAPI()
 
 app.include_router(router, prefix="/api", tags=["api"])
 # app.include_router(user_router, prefix="/api/stats", tags=["users"])
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+
+
